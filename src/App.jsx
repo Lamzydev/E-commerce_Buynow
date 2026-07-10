3c6ee0b6d5c8aa8f23a36d88e025a54d5feb79a5
Lamzydev <hallamublessin1994@gmail.com>
2026-07-10 15:52:56 +0100

    checkout update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import {Routes, Route} from 'react-router'
 import {HomePage} from './pages/HomePage'
+import {CheckoutPage} from './pages/CheckoutPage'
 import Navbar from './components/Navbar'
 import './App.css'
 
@@ -14,6 +15,8 @@ function App() {
      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/orders" element={<div>Orders Page</div>} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     
       </> 
